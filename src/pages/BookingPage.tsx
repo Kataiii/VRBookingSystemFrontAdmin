8fa3abfcb1498dfa193035e1fcfc92ac5cf07b7f
katya <katya_yuneva@mail.ru>
2024-01-16 13:58:04 +0400

    add navigate panel

---
+++ src/pages/BookingPage.tsx
@@ -0,0 +1,9 @@
+const BookingPage: React.FC = () => {
+    return(
+        <div>
+            <h1>BookingPage</h1>
+        </div>
+    )
+}
+
+export default BookingPage;
---
+++ src/pages/RulesPage.tsx
@@ -0,0 +1,9 @@
+const RulesPage: React.FC = () => {
+    return(
+        <div>
+            <h1>RulesPage</h1>
+        </div>
+    )
+}
+
+export default RulesPage;
