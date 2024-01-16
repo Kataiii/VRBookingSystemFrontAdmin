7cced007e37beaadd1d0e6eaea493ad7c381d848
katya <katya_yuneva@mail.ru>
2024-01-16 22:28:50 +0400

    add header for table

--- src/store/RoomStore.ts
+++ src/store/RoomStore.ts
@@ -8,31 +8,27 @@ import $api, { API_URL } from "../http/Index";
 
 export default class RoomStore{
     allRooms: IRoom[] = [];
-    notOccupiedRooms: IRoom[] = [];
 
     constructor(){
         makeAutoObservable(this);
     }
 
-    setAllRooms(rooms: IRoom[]){
+    setAllRooms = (rooms: IRoom[]) => {
         this.allRooms = rooms;
-    }
-
-    setNotOccupiedRooms(rooms: IRoom[]){
-        this.notOccupiedRooms = rooms;
     }
 
     getRooms = async (token: string) => {
         try{
-            const response: AxiosResponse<RoomGetResponce | any> = await $api.get(`/rooms`);
-            const rooms : IRoom[] = response.data.rooms;
+            const response = await $api.get(`/rooms`);
+            const rooms : IRoom[] = response.data;
+            console.log(rooms);
             this.setAllRooms(rooms);
-
-            this.setNotOccupiedRooms(rooms.filter((item) => item.isOccupied == false));
-            return this.notOccupiedRooms;
+            console.log('aaaaaaaaaa');
+            console.log(this.allRooms);
+            console.log(this);
+            return this.allRooms;
         }
         catch(e: any){
-            console.log(e.response?.status);
             return null;
         }   
     }
--- src/widgets/BookingTable.tsx
+++ src/widgets/BookingTable.tsx
@@ -1,21 +1,65 @@
-import { Table } from "antd";
+import { Table, TableProps } from "antd";
 import { observer } from "mobx-react-lite";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
 import { Context } from "..";
+import IRoom from "../enemy/Room";
+import LoadingPage from "../pages/LoadingPage";
+
+interface DataType {
+    key: string;
+    name: string
+}
+
+const data: DataType[] = [
+    {
+        key: '1',
+        name: 'John Brown'
+    },
+    {
+        key: '2',
+        name: 'Jim Green'
+    },
+    {
+        key: '3',
+        name: 'Joe Black'
+    },
+    {
+        key: '4',
+        name: 'Jim Red'
+    },
+    {
+        key: '5',
+        name: 'Jake White'
+    },
+];
 
 const BookingTable: React.FC = observer(() => {
     const { store, rooms } = useContext(Context);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    const cols = useMemo<TableProps<DataType>['columns']>(() => [
+        {
+            title: 'Время',
+            dataIndex: 'time',
+            rowScope: 'row',
+        },
+        ...rooms.allRooms.map(room => ({
+            title: room.name,
+            dataIndex: room.id,
+            render: (text: string) => <p>{text}</p>
+        }))
+    ], [rooms.allRooms]);
 
     useEffect(() => {
         const responce = rooms.getRooms(store.access);
-        responce.then((responce) => {
-        })
-    }, [])
+        responce.then((_) =>  setIsLoading(false));
+    }, []);
 
-    return(
-        <div></div>
-        // <Table columns={columns} dataSource={data} bordered />
+    return (
+        isLoading
+            ? <LoadingPage />
+            : <Table columns={cols} dataSource={data} bordered />
     )
-})
+});
 
 export default BookingTable;
